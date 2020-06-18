b5cd67879f0e3b9c87d023e6c26b3ef0999bbf8d
Jason <codejme@gmail.com>
2020-06-18 11:49:35 -0700

    vote handler added partial complete

---
+++ upvote/services/hosted/vote-lambda/index.js
@@ -0,0 +1,35 @@
+const AWS = require('aws-sdk');
+const documentClient = new AWS.DynamoDB.DocumentClient({ region: 'us-east-2' });
+const jwt = require('jsonwebtoken');
+const axios = require('axios');
+
+const verifyAndDecode = (auth) => {
+    const bearerPrefix = 'Bearer ';
+    if (!auth.startsWith(bearerPrefix)) return { err: 'Invalid authorization header' };
+    try {
+      const token = auth.substring(bearerPrefix.length);
+      const secret = process.env.secret;
+      return jwt.verify(token, Buffer.from(secret, 'base64'), { algorithms: ['HS256'] });
+    } catch (err) {
+      return { err: 'Invalid JWT' };
+    }
+};
+const voteHandler = async() =>{
+
+}
+exports.handler = async event => {
+    // Response function
+    const response = (statusCode, body) => {
+      const headers = {
+        ['Access-Control-Allow-Origin']: event.headers.origin,
+        ["Access-Control-Allow-Credentials"] : true
+      };
+  
+      return { statusCode, body: JSON.stringify(body, null, 2), headers };
+    };
+    const payload = verifyAndDecode(event.headers.Authorization);
+    const channelId = payload.channel_id;
+    const data = event['body'];
+    var res = await voteHandler(channelId, data)
+    return response(200, 'message--sucess');
+};
